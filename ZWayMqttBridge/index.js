3f3c4e51b44df3c4e5caa3c83f786beec848517a
David Uebelacker <david@uebelacker.ch>
2016-01-06 17:08:07 +0100

    devel

--- ZWayMqttBridge/index.js
+++ ZWayMqttBridge/index.js
@@ -4,7 +4,7 @@
  Author: David Uebelacker <david@uebelacker.ch>
  ******************************************************************************/
 
-function ZWayMqttBridge (id, controller) {
+function ZWayMqttBridge(id, controller) {
     ZWayMqttBridge.super_.call(this, id, controller);
 }
 
@@ -17,39 +17,35 @@ ZWayMqttBridge.prototype.init = function (config) {
 
     var self = this;
 
-
     this.deviceUpdate = function (device) {
         if (!self.mqttBridge) {
-            self.connect();
+            console.log('Connecting to Mqtt Bridge...');
+            self.mqttBridge = new sockets.websocket('ws://192.168.0.62:8080');
+
+            self.mqttBridge.onopen = function () {
+                console.log('Mqtt Bridge websocket connected!');
+            };
+
+            self.mqttBridge.onmessage = function (ev) {
+                console.log('got data:' + ev.data);
+            };
+
+            self.mqttBridge.onclose = function () {
+                console.log('Mqtt Bridge websocket was closed!');
+                self.mqttBridge = null;
+            };
+
+            self.mqttBridge.onerror = function (ev) {
+                console.log('Mqtt Bridge websocket error: ' + ev.data);
+            };
         }
+
         if (self.mqttBridge) {
             self.mqttBridge.send(JSON.stringify(device, null, 4));
         }
     };
 
     this.controller.devices.on('change:metrics:level', self.deviceUpdate);
-};
-
-ZWayMqttBridge.prototype.connect = function () {
-
-    this.mqttBridge = new sockets.websocket('ws://192.168.0.62:8080');
-
-    this.mqttBridge.onopen = function() {
-        console.log('Mqtt Bridge websocket connected!');
-    };
-
-    this.mqttBridge.onmessage = function(ev) {
-        console.log('got data:' + ev.data);
-    };
-
-    this.mqttBridge.onclose = function() {
-        console.log('Mqtt Bridge websocket was closed!');
-    };
-
-    this.onerror = function (ev) {
-        console.log('Mqtt Bridge websocket error: ' + ev.data);
-    };
-
 };
 
 ZWayMqttBridge.prototype.stop = function () {
