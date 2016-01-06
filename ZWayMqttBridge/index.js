d2b5dfb0261494888a24037188878506cbf91cbf
David Uebelacker <david@uebelacker.ch>
2016-01-06 18:04:09 +0100

    devel

--- ZWayMqttBridge/index.js
+++ ZWayMqttBridge/index.js
@@ -17,6 +17,48 @@ ZWayMqttBridge.prototype.init = function (config) {
 
     var self = this;
 
+    this.reconnect = function () {
+        self.connected = false;
+        self.mqttBridge = null;
+        console.log('Will try to reconnect to Mqtt Bridge in 10 seconds ...');
+        setTimeout(function() {
+            console.log('Will try to reconnect to Mqtt Bridge ...');
+            self.connect();
+        }, 10000);
+    }
+
+    this.connect = function () {
+        console.log('Connecting to Mqtt Bridge ....');
+        this.mqttBridge = new sockets.websocket('ws://192.168.0.62:8080');
+
+        setTimeout(function () {
+            if (self.mqttBridge && !self.connected) {
+                console.log('Could not connect to Mqtt Bridge after 5 seconds!');
+                self.reconnect();
+            }
+        }, 5000);
+
+        this.mqttBridge.onopen = function () {
+            console.log('Mqtt Bridge websocket connected!');
+            self.connected = true;
+        };
+
+        this.mqttBridge.onmessage = function (ev) {
+            console.log('got data:' + ev.data);
+        };
+
+        this.mqttBridge.onclose = function () {
+            console.log('Mqtt Bridge websocket was closed!');
+            self.reconnect();
+        };
+
+        this.mqttBridge.onerror = function (ev) {
+            console.log('Mqtt Bridge websocket error: ' + ev.data);
+            self.reconnect();
+        };
+    }
+
+
     this.deviceUpdate = function (device) {
         if (self.mqttBridge) {
             self.mqttBridge.send(JSON.stringify(device, null, 4));
@@ -27,53 +69,3 @@ ZWayMqttBridge.prototype.init = function (config) {
     this.controller.devices.on('change:metrics:level', self.deviceUpdate);
 };
 
-ZWayMqttBridge.prototype.reconnect = function () {
-    var self = this;
-    self.connected = false;
-    self.mqttBridge = null;
-    console.log('Will try to reconnect to Mqtt Bridge in 10 seconds ...');
-    setTimeout(function() {
-        console.log('Will try to reconnect to Mqtt Bridge ...');
-        self.connect();
-    }, 10000);
-}
-
-ZWayMqttBridge.prototype.connect = function () {
-    console.log('Connecting to Mqtt Bridge ....');
-    var self = this;
-    this.mqttBridge = new sockets.websocket('ws://192.168.0.62:8080');
-
-    setTimeout(function() {
-        if (self.mqttBridge && !self.connected) {
-            console.log('Could not connect to Mqtt Bridge after 5 seconds!');
-            self.reconnect();
-        }
-    }, 5000);
-
-    this.mqttBridge.onopen = function () {
-        console.log('Mqtt Bridge websocket connected!');
-        self.connected = true;
-    };
-
-    this.mqttBridge.onmessage = function (ev) {
-        console.log('got data:' + ev.data);
-    };
-
-    this.mqttBridge.onclose = function () {
-        console.log('Mqtt Bridge websocket was closed!');
-        self.reconnect();
-    };
-
-    this.mqttBridge.onerror = function (ev) {
-        console.log('Mqtt Bridge websocket error: ' + ev.data);
-        self.reconnect();
-    };
-};
-
-ZWayMqttBridge.prototype.stop = function () {
-    var self = this;
-    if (self.mqttBridge) {
-        self.mqttBridge.close();
-        self.mqttBridge = null;
-    }
-};
