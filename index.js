8e828705b80f04339b0c36f4b211f675419205e6
David Uebelacker <david@uebelacker.ch>
2016-01-06 21:51:24 +0100

    devel

--- index.js
+++ index.js
@@ -1,67 +1,21 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
 var mqtt = require('mqtt');
+var net = require('net');
 
-/*
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
-
-wsServer = new WebSocketServer({httpServer: server, autoAcceptConnections: true});
-
-wsServer.on('request', function(request) {
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-});
-
-*/
-
-
-
-var net = require('net'),
-    JsonSocket = require('json-socket');
-
-var port = 8080;
+var client  = mqtt.connect('mqtt://192.168.0.210');
 var server = net.createServer();
-server.listen(port);
+
+
 server.on('connection', function(socket) { //This is a standard net.Socket
     console.log('Got a client!');
     socket.write('Welcome to ZWayMqttBridge!\r\n');
     socket.on('data', function (data) {
         console.log(data.toString());
+        var message = JSON.parse(data.toString());
+        if (message && message.payload) {
+            client.publish(message.topic, message.payload.toString());
+        }
     });
-
 });
 
 
-var client  = mqtt.connect('mqtt://192.168.0.210');
-
-client.on('connect', function () {
-    client.subscribe('presence');
-    client.publish('presence', 'Hello mqtt');
-});
-
-client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString());
-
-});
+server.listen(8080);
